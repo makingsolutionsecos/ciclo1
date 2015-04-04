a0b67c4c214eff8b4d996acc4eaefac50561bbce
johans-123@hotmail.com <johans-123@hotmail.com>
2015-04-04 11:24:17 -0500

    ADD(jagonzalez25) - Clases
    Se añade la clase Analizador y Buscardor en su primera versión. Ciclo 1

---
+++ ciclo1ms/src/main/java/edu/uniandes/ms/modelo/Analizador.java
@@ -0,0 +1,41 @@
+
+package modelo;
+
+import dto.RespuestaAnalisis;
+import dto.RespuestaConteo;
+import java.io.File;
+import java.util.LinkedList;
+
+/**
+ * Clase encargada de generar un análisis por cada archivo de un directorio y extensión definido.
+ * @author Johans Gonzalez
+ * @version 1.0
+ */
+
+public class Analizador {
+    
+    /**
+     * Método encargado de retornar un objeto con el análisis desarrollado.
+     * @param ruta String Variable que contiene la ubicación del directorio a examinar.
+     * @param lenguaje LenguajeEnum Objeto que contiene el lenguaje que determina la extensión y lineamientos para desarrollar el análisis.
+     * @return un objeto de tipo RespuestaAnalisis con el análisis desarrollado.
+     */
+    public RespuestaAnalisis analizarDirectorioFuente(String ruta, LenguajeEnum lenguaje) {
+        RespuestaAnalisis respuestaAnalisis = new RespuestaAnalisis();
+
+        //Buscar Archivos
+        Buscador buscador = new Buscador();
+        LinkedList<File> archivos = buscador.getArchivos(ruta);
+        RespuestaConteo respuesta = new RespuestaConteo();
+
+        //Conteo
+        for (File archivo : archivos) {
+            IContador contador = FabricaContador.getInstancia(lenguaje);
+            contador.ejecutarConteo(archivo, respuesta);
+        }
+        
+        respuestaAnalisis.setRespuestaConteo(respuesta);
+        
+        return respuestaAnalisis;
+    }
+}
---
+++ ciclo1ms/src/main/java/edu/uniandes/ms/modelo/Buscador.java
@@ -0,0 +1,43 @@
+package modelo;
+
+import java.io.File;
+import java.util.LinkedList;
+
+/**
+ * Clase encargada de buscar los archivos  en un directorio con una extensión específica.
+ * @author Johans Gonzalez
+ * @version 1.0
+ */
+public class Buscador {
+    
+    /**
+     * Método encargado de retornar  el listado de archivos de un directorio.
+     * @param path String Variable que contiene la ubicación del directorio a examinar.
+     * @return Retorna listado de archivos de un directorio.
+     */    
+    public LinkedList<File> getArchivos(String path) {
+        File dir = new File(path);
+        return getArchivosDirectorio(dir);
+    }
+    
+    /**
+     * Método encargado de buscar los archivos de una extensión especifica  en un directorio.
+     * @param dir File Variable que contiene el directorio en el cual se desarrollara la búsqueda.
+     * @return Retorna listado de archivos de un directorio.
+     */    
+    private LinkedList<File> getArchivosDirectorio(File dir) {
+        LinkedList<File> archivos = new LinkedList<>() ;
+        File listFile[] = dir.listFiles();
+        if (listFile != null) {
+            for (File archivo : listFile) {
+                if (archivo.isDirectory()) {
+                    archivos.addAll(getArchivosDirectorio(archivo)) ;
+                } else if(archivo.getName().endsWith(".java")) {
+                    archivos.add(archivo);
+//                    System.out.println(archivo.getAbsolutePath());
+                }
+            }
+        }
+        return archivos;
+    }
+}
