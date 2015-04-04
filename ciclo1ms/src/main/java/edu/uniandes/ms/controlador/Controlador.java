230ae7d1d8d196e4fd12c5a9768dea34e3401c58
jorgealfonsobeltran <jorgealfonsobeltran@gmial.com>
2015-04-04 14:21:32 -0500

    ADD (jorealfonsobeltran) Controlador y LenguajeEnum
    Se agregaron los comentarios

---
+++ ciclo1ms/src/main/java/edu/uniandes/ms/controlador/Controlador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import dto.RespuestaAnalisis;
+import modelo.Analizador;
+import modelo.LenguajeEnum;
+
+/**
+ * Clase Controlador que llama a el analizador y ejecuta el analisis de un directorio
+ * @author Jorge Beltrán
+ * @version 1.0
+ * Fecha 01/04/2015
+ */
+public class Controlador {
+    public RespuestaAnalisis analizarDirectorioFuente(String ruta, LenguajeEnum lenguaje) {
+        Analizador analizador = new Analizador();
+        return analizador.analizarDirectorioFuente(ruta, lenguaje);
+    }
+}
---
+++ ciclo1ms/src/main/java/edu/uniandes/ms/modelo/LenguajeEnum.java
@@ -0,0 +1,15 @@
+package modelo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Enumeración que contiene los tipos de lenguaje a utilizar
+ * @author Jorge Beltrán
+ * @version 1.0
+ * Fecha 01/04/2015
+ */
+    public enum LenguajeEnum {JAVA, CSHARP, PHYTON}
