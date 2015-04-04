c0d25b8bec465168dbf3842965370360a9443033
John Dany Osorio Ramos <johnoso27@hotmail.com>
2015-04-04 15:26:48 -0500

    ADD(johnoso27)
    Interfaz que contiente las definiciones de un grupo de funciones
    relacionadas
    * con la solicitud y generación del análisis de código.

---
+++ ciclo1ms/src/main/java/edu/uniandes/ms/vista/IVista.java
@@ -0,0 +1,28 @@
+package edu.uniandes.ms.vista;
+
+import edu.uniandes.ms.dto.RespuestaAnalisis;
+
+/**
+ * Interfaz que contiente las definiciones de un grupo de funciones relacionadas
+ * con la solicitud y generación del análisis de código.
+ *
+ * @author John Dany osorio Ramos
+ * @version 1.0
+ */
+public interface IVista {
+
+    /**
+     * Define el método que analiza el código fuente de un lenguaje en
+     * específico y se optiene su respectivo análisis.
+     */
+    public void analizarDirectorioFuente();
+
+    /**
+     * Define el método encargado de pintar la respuesta del análisis del código
+     * fuente analizado.
+     *
+     * @param respuestaAnalisis Respuesta que contiene el análisis detallado del
+     * código fuente analizado.
+     */
+    public void pintar(RespuestaAnalisis respuestaAnalisis);
+}
